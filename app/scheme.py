dc4af5ed4c7148d9d18d08583bcaf6b81578482f
einspaten <tassentanne@gmail.com>
2019-05-27 18:18:11 +0200

    added scheme

---
+++ app/scheme.py
@@ -0,0 +1,23 @@
+invalid_device: dict = {'error': 'invalid device uuid'}
+
+invalid_file: dict = {'error': 'invalid file uuid'}
+
+alread_have_device: dict = {'error': 'you already own a device'}
+
+permission_denied: dict = {'error': 'no access to this device'}
+
+no_name: dict = {'error': 'no name given'}
+
+success: dict = {'ok': True}
+
+this_device_does_not_exists: dict = {"error": "this device does not exists"}
+
+no_content: dict = {'error': 'no content given'}
+
+no_file_name: dict = {'error': 'no filename given'}
+
+length_exceeded: dict = {'file length exceeded'}
+
+no_file: dict = {'error': 'no file with this name exist'}
+
+file_already_exists: dict = {'filename already taken'}
