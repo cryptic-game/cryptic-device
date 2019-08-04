4dc99792cb8b7054b77385b87e41a305c0f735a6
Digging Tool <espriworkemail@gmail.com>
2019-08-04 14:16:09 +0200

    reverted imports(#37)

--- app/app.py
+++ app/app.py
@@ -7,9 +7,9 @@ m: MicroService = MicroService("device")
 wrapper: DatabaseWrapper = m.get_wrapper()
 
 if __name__ == "__main__":
-    import resources.device
-    import resources.file
-    import resources.hardware
+    from resources.device import *
+    from resources.file import *
+    from resources.hardware import *
 
     wrapper.Base.metadata.create_all(bind=wrapper.engine)
     m.run()
