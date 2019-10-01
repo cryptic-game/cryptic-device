8969e730975061c405d807cb72375bc7341b7049
Defelo <elodef42@gmail.com>
2019-10-01 23:42:09 +0200

    added unit test for delete_user endpoint (#59)

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -81,7 +81,7 @@ class TestApp(TestCase):
             ["hardware", "register"],
             ["hardware", "stop"],
             ["hardware", "scale"],
-            (["delete_user"]),
+            ["delete_user"],
         ]
 
         for path, requires in expected_user_endpoints:
