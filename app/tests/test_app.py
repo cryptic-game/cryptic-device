00fe0ed836a17b149b60365a2b790fe0d3a29ea9
Defelo <elodef42@gmail.com>
2019-10-02 00:21:18 +0200

    improved endpoints_available unit test (#62)

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -2,6 +2,7 @@ from importlib import machinery, util
 from unittest import TestCase
 
 from mock.mock_loader import mock
+from resources import device, file, hardware
 from schemes import (
     requirement_device,
     requirement_build,
@@ -54,45 +55,47 @@ class TestApp(TestCase):
         registered_ms_endpoints = mock.ms_endpoints.copy()
 
         expected_user_endpoints = [
-            (["device", "info"], requirement_device),
-            (["device", "ping"], requirement_device),
-            (["device", "all"], {}),
-            (["device", "create"], requirement_build),
-            (["device", "starter_device"], {}),
-            (["device", "power"], requirement_device),
-            (["device", "change_name"], requirement_change_name),
-            (["device", "delete"], requirement_device),
-            (["device", "spot"], {}),
-            (["file", "all"], requirement_device),
-            (["file", "info"], requirement_file),
-            (["file", "move"], requirement_file_move),
-            (["file", "update"], requirement_file_update),
-            (["file", "delete"], requirement_file),
-            (["file", "create"], requirement_file_create),
-            (["hardware", "build"], requirement_build),
-            (["hardware", "resources"], requirement_device),
-            (["hardware", "process"], requirement_service),
-            (["hardware", "list"], {}),
+            (["device", "info"], requirement_device, device.device_info),
+            (["device", "ping"], requirement_device, device.ping),
+            (["device", "all"], {}, device.list_devices),
+            (["device", "create"], requirement_build, device.create_device),
+            (["device", "starter_device"], {}, device.starter_device),
+            (["device", "power"], requirement_device, device.power),
+            (["device", "change_name"], requirement_change_name, device.change_name),
+            (["device", "delete"], requirement_device, device.delete_device),
+            (["device", "spot"], {}, device.spot),
+            (["file", "all"], requirement_device, file.list_files),
+            (["file", "info"], requirement_file, file.file_info),
+            (["file", "move"], requirement_file_move, file.move),
+            (["file", "update"], requirement_file_update, file.update),
+            (["file", "delete"], requirement_file, file.delete_file),
+            (["file", "create"], requirement_file_create, file.create_file),
+            (["hardware", "build"], requirement_build, hardware.build),
+            (["hardware", "resources"], requirement_device, hardware.hardware_resources),
+            (["hardware", "process"], requirement_service, hardware.hardware_process),
+            (["hardware", "list"], {}, hardware.hardware_list),
         ]
 
         expected_ms_endpoints = [
-            ["exist"],
-            ["owner"],
-            ["hardware", "register"],
-            ["hardware", "stop"],
-            ["hardware", "scale"],
-            ["delete_user"],
+            (["exist"], device.exist),
+            (["owner"], device.owner),
+            (["hardware", "register"], hardware.hardware_register),
+            (["hardware", "stop"], hardware.hardware_stop),
+            (["hardware", "scale"], hardware.hardware_scale),
+            (["delete_user"], device.delete_user),
         ]
 
-        for path, requires in expected_user_endpoints:
+        for path, requires, func in expected_user_endpoints:
             self.assertIn((path, requires), registered_user_endpoints)
             registered_user_endpoints.remove((path, requires))
             self.assertIn(mock.user_endpoint_handlers[tuple(path)], elements)
+            self.assertEqual(func, mock.user_endpoint_handlers[tuple(path)])
 
-        for path in expected_ms_endpoints:
+        for path, func in expected_ms_endpoints:
             self.assertIn(path, registered_ms_endpoints)
             registered_ms_endpoints.remove(path)
             self.assertIn(mock.ms_endpoint_handlers[tuple(path)], elements)
+            self.assertEqual(func, mock.ms_endpoint_handlers[tuple(path)])
 
         self.assertFalse(registered_user_endpoints)
         self.assertFalse(registered_ms_endpoints)
